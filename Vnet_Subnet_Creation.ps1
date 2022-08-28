f2981cb421e172dbc99680dd2b3fdaa59cf3372a
= <=>
2022-08-28 22:51:28 +0530

    Changes done

--- Vnet_Subnet_Creation.ps1
+++ Vnet_Subnet_Creation.ps1
@@ -1,3 +1,34 @@
+# param(
+#     [Parameter(Mandatory=$true)][string] $RGLocation,
+#     [Parameter(Mandatory=$true)][string] $RGName,
+#     [Parameter(Mandatory=$true)][string] $RVnetName,
+#     [Parameter(Mandatory=$true)][string] $VnetRange,
+#     [Parameter(Mandatory=$true)][string] $SubnetName,
+#     [Parameter(Mandatory=$true)][string] $SubnetRange
+# )
+# $ErrorActionPreference  = "stop"
+# [console]::ResetColor()
+# $ScriptName = $MyInvocation.MyCommand.Name
+
+# Write-host "----------------------------------------------------"
+# Write-Host resourcegroup location is $RGLocation
+# Write-Host resourcegroup name is $RGName
+# Write-host "----------------------------------------------------"
+# #Check RG Status
+# $RGStatus = az group exists -n $RGName
+
+# if ($RGStatus -eq $true)
+# {
+#     az network vnet create -g $RGName -n $RVnetName--address-prefix $VnetRange --subnet-name $SubnetName --subnet-prefix $SubnetRange -l $RGLocation
+# }
+# else{
+#     Write-Host Network $RGStatus already exists
+# }
+
+# #======================
+# Write-Host "End of Script ($ScriptName)"
+
+
 param(
     [Parameter(Mandatory=$true)][string] $RGLocation,
     [Parameter(Mandatory=$true)][string] $RGName,
@@ -19,10 +50,12 @@ $RGStatus = az group exists -n $RGName
 
 if ($RGStatus -eq $true)
 {
-    az network vnet create -n $RVnetName--address-prefix $VnetRange --subnet-name $SubnetName --subnet-prefix $SubnetRange -l centralindia
+    az network vnet create -g $RGName -n $RVnetName --address-prefix $VnetRange --subnet-name $SubnetName --subnet-prefix $SubnetRange -l $RGLocation
 }
-else{
-    Write-Host resourcegroup $RGName already exists
+
+else {
+    write-Host Network $RGStatus already exists
 }
 #======================
 Write-Host "End of Script ($ScriptName)"
+
